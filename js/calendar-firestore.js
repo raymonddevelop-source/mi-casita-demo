d1873d2e2284e6379d9d4578619f74981305502f
raymonddevelop-source <raymonddevelop@gmail.com>
2026-03-01 21:59:53 -0400

    Create calendar-firestore.js

---
+++ js/calendar-firestore.js
@@ -0,0 +1,14 @@
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
+export async function loadBlockedDates() {
+  const blocked = new Set();
+  const snap = await getDocs(collection(db, "calendar"));
+
+  snap.forEach((doc) => {
+    const data = doc.data();
+    if (data && typeof data.date === "string") blocked.add(data.date);
+  });
+
+  return blocked;
+}
