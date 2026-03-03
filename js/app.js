ab30dea7d69294b17fa8ea61b048cd46f687667c
raymonddevelop-source <raymonddevelop@gmail.com>
2026-03-02 23:17:25 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import { loadBlockedDates } from "./calendar-firestore.js";
 import { createBooking } from "./bookings-firestore.js";
 import { blockDates } from "./calendar-write-firestore.js";
+import { holdDatesIfFree } from "./calendar-transaction-firestore.js";
 
 console.log("✅ app.js cargó");
 
